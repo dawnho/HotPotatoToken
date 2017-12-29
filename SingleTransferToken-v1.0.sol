4b5b67d5ee3171ff2b526f22f781583b407693dc
Dawn Ho <dawn.ho@gmail.com>
2017-12-28 22:45:29 -0500

    ERC tweaks

--- SingleTransferToken-v1.0.sol
+++ SingleTransferToken-v1.0.sol
@@ -1,15 +1,18 @@
 pragma solidity ^0.4.8;
 
+
 /// @title Interface for contracts conforming to ERC-721: Non-Fungible Tokens
 /// @author Dieter Shirley <dete@axiomzen.co> (https://github.com/dete)
 contract ERC721 {
-    function implementsERC721() public pure returns (bool);
-    function totalSupply() public view returns (uint256 total);
+    // Required methods
+    function approve(address _to, uint256 _tokenId) public;
     function balanceOf(address _owner) public view returns (uint256 balance);
-
-    function approve(address _to, uint256 _tokenId) public returns(bool success);
-    function transferFrom(address _from, address _to, uint256 _tokenId) public returns(bool success);
-    function transfer(address _to, uint256 _tokenId) public returns(bool success);
+    function implementsERC721() public pure returns (bool);
+    function ownerOf(uint256 _tokenId) public view returns (address owner);
+    function takeOwnership(uint256 _tokenId) public;
+    function totalSupply() public view returns (uint256 total);
+    function transferFrom(address _from, address _to, uint256 _tokenId) public;
+    function transfer(address _to, uint256 _tokenId) public;
 
     event Transfer(address indexed from, address indexed to, uint256 amount);
     event Approval(address indexed owner, address indexed approved, uint256 amount);
@@ -21,25 +24,26 @@ contract ERC721 {
     // function tokenMetadata(uint256 _tokenId) public view returns (string infoUrl);
 }
 
+
 contract SingleTransferToken is ERC721 {
 
-    string public symbol = "STT";
+    /// @notice Name and symbol of the non fungible token, as defined in ERC721.
+    string private _name = "Single Transfer Token";
+    string private _symbol = "STT";
 
-    string public name = "SingleTransferToken";
-
-    uint256 _totalSupply = 1;
+    uint256 private constant TOTAL_SUPPLY = 1;
+    uint256 private constant TOKEN_ID = 2046;
 
     uint256 currentPrice;
 
     uint256 sellingPrice;
 
-    uint256 stepLimit = 1 ether;
+    uint256 private stepLimit = 2 ether;
 
     event Transfer(address indexed from, address indexed to, uint256 amount);
 
     // Owner of this contract
-
-    address owner;
+    address private contractOwner;
 
     // Current owner of the token
     address public tokenOwner;
@@ -47,32 +51,32 @@ contract SingleTransferToken is ERC721 {
     // Allowed to transfer to this address
     address allowedTo = address(0);
 
-    modifier onlyOwner() {
-
-        require(msg.sender == owner);
-        _;
-
-    }
-
-    modifier onlySingle(uint256 amount){
-        require(amount == 1);
+    /// Access modifier for contract owner only functionality
+    modifier onlyContractOwner() {
+        require(msg.sender == contractOwner);
         _;
     }
 
-    function implementsERC721() public pure returns (bool)
-    {
-        return true;
+    /// Access modifier for token owner only functionality
+    modifier onlyTokenOwner() {
+        require(msg.sender == tokenOwner);
+        _;
+    }
+
+    /// Verifying that tokenId is valid
+    modifier tokenIdMatches(uint256 _tokenId) {
+        require(_tokenId == TOKEN_ID);
+        _;
     }
 
     // Constructor
+    function SingleTransferToken(string tokenName, string tokenSymbol, uint256 initialPrice, uint256 sLimit) public {
 
-    function SingleTransferToken(string tokenName, string tokenSymbol, uint256 initialPrice, uint256 sLimit) public{
+        _name = tokenName;
 
-        name = tokenName;
+        _symbol = tokenSymbol;
 
-        symbol = tokenSymbol;
-
-        owner = msg.sender;
+        contractOwner = msg.sender;
 
         tokenOwner = msg.sender;
 
@@ -84,87 +88,99 @@ contract SingleTransferToken is ERC721 {
 
     }
 
-    function totalSupply() constant public returns (uint256 total) {
-
-        total = _totalSupply;
-
-    }
-
-    // What is the balance of a particular account?
-
-    function balanceOf(address _owner) constant public returns (uint256 balance) {
-
-        return _owner == tokenOwner ? 1 : 0;
-
-    }
-
-    // Transfer the balance from owner's account to another account
-
-    function transfer(address _to, uint256 _amount) onlySingle(_amount) public returns (bool success) {
-
-        if(balanceOf(msg.sender) > 0){
-
-            tokenOwner = _to;
-
-            Transfer(msg.sender, _to, _amount);
-
-            success = true;
-
-        } else {
-
-            success = false;
-
-        }
-
-    }
-
-    // Send _value amount of tokens from address _from to address _to
-
-    function transferFrom(
-
-        address _from,
-
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    // If this function is called again it overwrites the current allowance with _value.
+    function approve(
         address _to,
+        uint256 _tokenId
+    ) public onlyTokenOwner tokenIdMatches(_tokenId) {
+        // Owner cannot grant approval to self.
+        require(msg.sender != _to);
 
-        uint256 _amount
+        allowedTo = _to;
 
-    ) onlySingle(_amount) public returns (bool success) {
+        Approval(msg.sender, _to, _tokenId);
+    }
 
-        require(balanceOf(_from) > 0 && allowedTo == _to);
+    //  Required for ERC-721 compliance.
+    //  For querying balance of a particular account
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return _owner == tokenOwner ? 1 : 0;
+    }
+
+    function implementsERC721() public pure returns (bool) {
+        return true;
+    }
+
+    function name() public view returns (string name) {
+        name = _name;
+    }
+
+    /// Required for ERC-721 compliance.
+    // For querying owner of token
+    function ownerOf(uint256 _tokenId)
+        public
+        view
+        tokenIdMatches(_tokenId)
+        returns (address owner)
+    {
+        owner = tokenOwner;
+    }
+
+    function symbol() public view returns (string symbol) {
+        symbol = _symbol;
+    }
+
+    function takeOwnership(uint256 _tokenId) public tokenIdMatches(_tokenId) {
+        address newOwner = msg.sender;
+
+        // Safety check to prevent against an unexpected 0x0 default.
+        require(newOwner != address(0));
+
+        require(approvalOf(newOwner));
+        require(newOwner != tokenOwner);
+        Transfer(tokenOwner, newOwner, _tokenId);
+    }
+
+    /// Required for ERC-721 compliance.
+    /// For querying totalSupply of token
+    function totalSupply() public view returns (uint256 total) {
+        return TOTAL_SUPPLY;
+    }
+
+    /// Required for ERC-721 compliance.
+    // Transfer the balance from owner's account to another account
+    function transfer(
+        address _to,
+        uint256 _tokenId
+    ) public  onlyTokenOwner tokenIdMatches(_tokenId) {
+        // Safety check to prevent against an unexpected 0x0 default.
+        require(newOwner != address(0));
 
         tokenOwner = _to;
-
-        Transfer(_from, _to, _amount);
-
-        success = true;
+        Transfer(msg.sender, _to, _tokenId);
     }
 
+    // Send _tokenId token from address _from to address _to
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public tokenIdMatches(_tokenId) {
+        oldOwner = _from;
+        newOwner = _to;
 
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+        // Safety check to prevent against an unexpected 0x0 default.
+        require(newOwner != address(0));
 
-    // If this function is called again it overwrites the current allowance with _value.
+        require(approvalOf(newOwner));
+        require(tokenOwner == oldOwner);
 
-    function approve(address _spender, uint256 _amount) public onlySingle(_amount) returns (bool success) {
-
-        require(tokenOwner == msg.sender);
-
-        allowedTo = _spender;
-
-        Approval(msg.sender, _spender, _amount);
-
-        success = true;
-
-    }
-
-
-    function allowance(address _owner, address _spender) constant public returns (uint256 remaining) {
-
-        return _owner == tokenOwner && allowedTo == _spender? 1 : 0;
-
+        tokenOwner = _to;
+        Transfer(_from, _to, _tokenId);
     }
 
     // Allows someone to send ether and obtain the token
-
     function() public payable {
 
         //making sure token owner is not sending
@@ -183,11 +199,11 @@ contract SingleTransferToken is ERC721 {
         //update prices
         currentPrice = sellingPrice;
 
-        if(currentPrice >= stepLimit){
+        if (currentPrice >= stepLimit) {
 
             sellingPrice = (currentPrice * 120)/94; //adding commission amount //1.2/(1-0.06)
 
-        }else{
+        } else {
 
             sellingPrice = (currentPrice * 2 * 100)/94;//adding commission amount
 
@@ -205,7 +221,22 @@ contract SingleTransferToken is ERC721 {
 
     }
 
-    function transferToken(address prevOwner, address newOwner) internal {
+    function payout(address _to) public onlyContractOwner {
+        if (this.balance > 1 ether) {
+            if (_to == address(0)) {
+                owner.transfer(this.balance - 1 ether);
+            } else {
+                _to.transfer(this.balance - 1 ether);
+            }
+        }
+    }
+
+    // Private functions
+    function approvalOf(address _to) private view returns (bool approved) {
+        return allowedTo == _to;
+    }
+
+    function transferToken(address prevOwner, address newOwner) private {
 
         //pay previous owner
         prevOwner.transfer((currentPrice*94)/100); //(1-0.06)
@@ -216,16 +247,4 @@ contract SingleTransferToken is ERC721 {
 
 
     }
-
-    function payout(address _to) onlyOwner public{
-    	if(this.balance > 1 ether){
-    		if(_to == address(0)){
-    			owner.transfer(this.balance - 1 ether);
-    		}else{
-    			_to.transfer(this.balance - 1 ether);
-    		}
-
-    	}
-    }
-
 }
